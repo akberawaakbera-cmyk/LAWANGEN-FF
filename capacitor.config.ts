60c4d20f0d490de4c33292454c89f59b28c5ac04
akberawaakbera-cmyk <akberawaakbera@gmail.com>
2026-09-26 19:23:02 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lawangen.adminpanel',
+  appName: 'LAWANGEN',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
